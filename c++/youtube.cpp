9e2379335345d45f2feb8d96a25a4ff974f8ebbd
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2022-05-22 15:01:37 +0100

    intro to creating custom functions

--- c++/youtube.cpp
+++ c++/youtube.cpp
@@ -3,13 +3,20 @@
 using std::cout;
 using std::cin;
 
+
+int multiply(int x, int y)
+{
+    return x * y;
+}
+
 int main()
 {
     int slices;
-
     cout << "How many slices of pizza would you like?: ";
-
     cin >> slices;
-    
-    cout << "You have " << slices << " slices of pizza." << std::endl; // C++ string concatenation
+    cout << "You have " << slices << " slices of pizza." << std::endl;
+
+    cout << "The number of your pizza slices has doubled, you now have " << multiply(slices, 2) << " pizza slices." << std::endl;
+
 }
+
