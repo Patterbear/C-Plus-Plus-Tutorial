fe7f20f781affaebb4ccbe163cae507aada51bd0
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2022-05-22 14:15:49 +0100

    user input with cin

--- c++/youtube.cpp
+++ c++/youtube.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 using std::cout;
+using std::cin;
 
 int main()
 {
-    int slices = 10;
+    int slices;
 
+    cout << "How many slices of pizza would you like?: ";
+
+    cin >> slices;
+    
     cout << "You have " << slices << " slices of pizza." << std::endl; // C++ string concatenation
-
-    printf("%i\n", slices); // C print statement
 }
