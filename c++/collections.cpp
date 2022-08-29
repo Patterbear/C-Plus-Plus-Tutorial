dca14cca7702fe12bbc5f6db3b4530d88fcfed1b
Patterbear <benjamin.mcgregor2002@gmail.com>
2022-08-29 15:12:35 +0100

    passing arrays to functions and sizeof operator

--- c++/collections.cpp
+++ c++/collections.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
+void print_array(int array[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << array[i] << "\t";
+    }
+}
+
 int main()
 {
     int guesses[] = {12, 43, 23, 43, 23};
+    int size = sizeof(guesses) / sizeof(int);
+    print_array(guesses, 5);
 
+    /*
     int num = 3;
     int size = sizeof(guesses) / sizeof(guesses[0]); // Calculates size of array, can use 'sizeof(int)' instead of 'sizeof(guesses[0])'
 
     std::cout << size << std::endl;
-
-    for(int i = 0; i < size; i++)
-    {
-        std::cout << guesses[i] << std::endl;
-    }
-
-        for(int i = 0; i < num; i++)
-    {
-        std::cout << guesses[i] << std::endl;
-    }
+    */
 
     return 0;
 }
