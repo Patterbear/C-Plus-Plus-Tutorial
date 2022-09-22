037eb27f8395d7f90132e8f6256609a888da473e
Patterbear <benjamin.mcgregor2002@gmail.com>
2022-09-22 11:58:09 +0100

    const modifier

--- c++/collections.cpp
+++ c++/collections.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <vector>
 
+void do_something(const int array[])
+{
+    // MUST NOT alter array as it has been labelled 'const'
+}
+
+void print_array(const int data[], int size) // 'const' ensures the actual array isn't altered (A 'promise' not to change the data)
+{
+    for(int i = 0; i < size; i++)
+    {
+        // data[i]++; // Won't work because we've labelled data 'const'
+        std::cout << data[i] << "\t";
+    }
+    std::cout << "\n";
+    do_something(data);
+}
+
 int main()
 {
-    int grades_array[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-    std::vector<std::vector<int>> grades =
-    {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-
-
-    // Nested for loop to iterate through multidimensional vector
-    for(int r = 0; r < 3; r++)
-    {
-        for(int c = 0; c < 3; c++)
-        {
-            std::cout << grades[r][c] << "\t";
-        }
-        std::cout << "\n";
-    }
+    int data[] = {1, 2, 3};
+    print_array(data, 3);
 
     return 0;
 }
--- c++/previous_code.cpp
+++ c++/previous_code.cpp
@@ -517,6 +517,29 @@ void range_based_for_loop()
     std::cout << "\n";
 }
 
+void multidimensional_arrays_and_nested_vectors()
+{
+        int grades_array[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    std::vector<std::vector<int>> grades =
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+
+    // Nested for loop to iterate through multidimensional vector
+    for(int r = 0; r < 3; r++)
+    {
+        for(int c = 0; c < 3; c++)
+        {
+            std::cout << grades[r][c] << "\t";
+        }
+        std::cout << "\n";
+    }
+}
+
 
 int main()
 { 
