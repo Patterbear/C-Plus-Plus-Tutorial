18afdc814fd042c96c20eb3cbc819eefc1e532cd
Patterbear <benjamin.mcgregor2002@gmail.com>
2022-09-22 13:14:33 +0100

    function overloading examples

--- c++/swap.cpp
+++ c++/swap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 void swap(int &a, int &b) // '&' ensures variable is passed by reference
 {
     int temp = a;
@@ -9,13 +10,34 @@ void swap(int &a, int &b) // '&' ensures variable is passed by reference
     std::cout << "a: " << a << "\tb: " << b << "\n";
 }
 
+
+void swap(std::string &a, std::string &b)
+{
+    std::string temp = a;
+    a = b;
+    b = temp;
+}
+
+
 int main()
 {
     int a = 10;
     int b = 20;
 
+    std::cout << "a: " << a << "\tb: " << b << "\n";
+
     swap(a, b);
 
-    std::cout << "a: " << a << "\tb: " << b << "\n";
+
+    std::string forename = "Benjamin";
+    std::string surname = "McGregor";
+
+    std::cout << forename << " " << surname << std::endl;
+
+    swap(forename, surname);
+
+    std::cout << forename << " " << surname << std::endl;
+
+    
     return 0;
 }
